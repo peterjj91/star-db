d24d065dbf7e9dbee7e54e5f9d5b369ab0320b15
peterjj91 <peterjj91@gmail.com>
2019-01-10 19:18:52 +0200

    Try fetch(), async and await.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello')
+
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+}
+
+getResource('https://swapi.co/api/people/1/')
+  .then((body) => {
+    console.log(body);
+  });
+
+// тот же код, что и наверху
+// fetch()
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((body) => {
+//     console.log(body)
+//   });
