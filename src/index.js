eb815e30e4f2e951c75b8d032152e0b9451d6533
peterjj91 <peterjj91@gmail.com>
2019-01-10 19:38:44 +0200

    Error processing for fetch.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  // обработка с ошибкой
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`);
+  }
+
   const body = await res.json();
   return body;
 }
 
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/people/13erfd/')
   .then((body) => {
     console.log(body);
+  })
+  .catch((err) => { // ловим ошибки 
+    console.log('Could not ', err)
   });
 
 // тот же код, что и наверху
