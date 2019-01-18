360a5958c6c8de431d4fbe471456d2d826867db3
peterjj91 <peterjj91@gmail.com>
2019-01-18 20:42:36 +0200

    Create component Row for people-page.

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -7,6 +7,19 @@ import ErrorIndicator from '../error-indicator/error-indicator';
 
 import './people-page.css';
 
+const Row = ({left, right}) => {
+  return (
+    <div className="row mb2">
+      <div className="col-md-6">
+        {left}
+      </div>
+      <div className="col-md-6">
+        {right}
+      </div>
+    </div>
+  );
+};
+
 export default class PeoplePage extends Component {
 
   swapiService = new SwapiService();
@@ -45,14 +58,7 @@ export default class PeoplePage extends Component {
     }
 
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          {itemList}
-        </div>
-        <div className="col-md-6">
-          {personDetails}
-        </div>
-      </div>
+      <Row left={itemList} right={personDetails} />
     );
   }
 }
