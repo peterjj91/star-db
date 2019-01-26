03147ef5e59280d964c850b3c7155eacadbfde7d
peterjj91 <peterjj91@gmail.com>
2019-01-26 09:32:08 +0200

    Refactoring and for item data

--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -1,108 +1,86 @@
 import React, { Component } from 'react';
 
-import Spinner from '../spinner';
-
+import ErrorButton from '../error-button/error-button';
 import SwapiService from '../../services/swapi-service';
-import ErrorButton from "../error-button/error-button";
+
 import './item-details.css';
+
+const Record = ({ item, field, label }) => {
+  return (
+    <li className="list-group-item">
+      <span className="term">{label}</span>
+      <span>{ item[field] }</span>
+    </li>
+  );
+};
+
+export {
+  Record
+};
+
 
 export default class ItemDetails extends Component {
 
   swapiService = new SwapiService();
 
   state = {
-    item: {},
-    loading: true, // загрузка 
-    error: false // на старте ошибок нет
+    item: null,
+    image: null
   };
 
   componentDidMount() {
     this.updateItem();
-  };
+  }
 
   componentDidUpdate(prevProps) {
-    if (this.props.itemId !== prevProps.itemId ) {
+    if (this.props.itemId !== prevProps.itemId) {
       this.updateItem();
     }
   }
 
   updateItem() {
-    const { itemId } = this.props;
+    const { itemId, getData, getImageUrl } = this.props;
     if (!itemId) {
       return;
     }
 
-    this.swapiService
-      .getItem(itemId)
-      .then(this.onItemLoaded)
-      .catch(this.onError);
+    getData(itemId)
+      .then((item) => {
+        this.setState({
+          item,
+          image: getImageUrl(item)
+        });
+      });
   }
-
-  onItemLoaded = (item) => {
-    this.setState({
-      item,
-      loading: false
-    })
-  };
-
-  // ошибки
-  onError = (err) => {
-    this.setState({
-      error: true,
-      loading: false
-    })
-  };
 
   render() {
 
-    if (!this.state.item) {
-      return <span>Select a person from a list</span>;
+    const { item, image } = this.state;
+    if (!item) {
+      return <span>Select a item from a list</span>;
     }
 
-    const { item, loading, error } = this.state; // деструктурировали необходимые данные 
-
-    const hasData = !(loading || error);
-
-    const spinner = loading ? <Spinner /> : null;
-    const content = hasData ? <ItemView item={item} /> : null;
+    const { id, name, gender,
+              birthYear, eyeColor } = item;
 
     return (
       <div className="item-details card">
-        {spinner}
-        {content}
+        <img className="item-image"
+          src={image}
+          alt="item"/>
+
+        <div className="card-body">
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            {
+              React.Children.map(this.props.children, (child) => {
+                return React.cloneElement(child, { item });
+              })
+            }
+          </ul>
+          <ErrorButton />
+        </div>
       </div>
-    )
+    );
   }
 }
-
-const ItemView = ({item}) => {
-  const { id, name, gender, birthYear, eyeColor } = item; // деструктурировали необходимые данные 
-
-  return (
-    <React.Fragment>
-      <img className="item-image"
-        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-        alt="character"/>
-
-      <div className="card-body">
-        <h4>{name}</h4>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <span className="term">Gender</span>
-            <span>{gender}</span>
-          </li>
-          <li className="list-group-item">
-            <span className="term">Birth Year</span>
-            <span>{birthYear}</span>
-          </li>
-          <li className="list-group-item">
-            <span className="term">Eye Color</span>
-            <span>{eyeColor}</span>
-          </li>
-        </ul>
-
-        <ErrorButton />
-      </div>
-    </React.Fragment>
-  );
-};
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import ItemList from '../item-list/item-list';
-import PersonDetails from '../person-details/person-details';
+import PersonDetails from '../item-details/item-details';
 import SwapiService from '../../services/swapi-service';
 import Row from '../row';
 import ErrorBoundry from '../error-boundry';
