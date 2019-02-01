029a01194fd20334f43eced42724fb7d163855e5
peterjj91 <peterjj91@gmail.com>
2019-02-01 22:54:51 +0200

    Relative paths for people page.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -41,7 +41,7 @@ export default class App extends Component {
               <RandomPlanet />
               
               <Route path="/" render={() => <h2>Welcome</h2>} exact />
-              <Route path="/people" component={PeoplePage} />
+              <Route path="/people/:id?" component={PeoplePage} />
               <Route path="/planets" component={PlanetsPage} />
               <Route path="/starships" exact component={StarshipsPage} />
               <Route path="/starships/:id" 
