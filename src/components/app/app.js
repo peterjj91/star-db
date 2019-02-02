b200dfeed55b22ada97afb21d900418162fccb25
peterjj91 <peterjj91@gmail.com>
2019-02-02 12:59:22 +0200

    Authorization with the help of Redirect

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,7 +6,13 @@ import ErrorBoundry from '../error-boundry';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 
-import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+import { 
+  PeoplePage, 
+  PlanetsPage, 
+  StarshipsPage,
+  LoginPage, 
+  SecretPage
+} from '../pages';
 import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
@@ -17,8 +23,15 @@ import { StarshipDetails } from '../sw-components';
 export default class App extends Component {
 
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
   };
+
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    })
+  }
 
   onServiceChange = () => {
     this.setState(({ swapiService }) => {
@@ -31,6 +44,7 @@ export default class App extends Component {
   };
 
   render() {
+    const { isLoggedIn } = this.state;
 
     return (
       <ErrorBoundry>
@@ -49,6 +63,18 @@ export default class App extends Component {
                        const { id } = match.params;
                        return <StarshipDetails itemId={id} />
                      }} />
+              <Route 
+                path="/login" 
+                render={() => (
+                  <LoginPage 
+                    isLoggedIn={isLoggedIn}
+                    onLogin={this.onLogin} />
+                )} />
+              <Route 
+                path="/secret"
+                render={() => (
+                  <SecretPage isLoggedIn={isLoggedIn} />
+                )} />
             </div>
           </Router>
         </SwapiServiceProvider>
